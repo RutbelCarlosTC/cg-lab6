a332ea37a02870c78a41432d43f16d2f1b2f131d
cdumasic <cumasic@unsa.edu.pe>
2025-06-08 23:42:47 -0500

    Se añadio una linterna y se realizo el bakeo simple

---
+++ Assets/Code/Linterna.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linterna : MonoBehaviour
+{
+    public Light flashlight;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            flashlight.enabled = !flashlight.enabled;
+        }
+    }
+}
